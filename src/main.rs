87a1301701c165e7c473d1c0829f064248525622
Ram Patel <69773020+p-328@users.noreply.github.com>
2023-01-06 17:55:20 +0000

    Change if statement to match statement

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ fn main() -> Result<(), String> {
   if let None = args.get(1) {
     return Err("Usage: cargo run <filename>".to_string())
   }
-  if let Err(e) = read_to_string(args.get(1).unwrap()) {
-    let msg = format!("{:?}", e);
-    return Err(msg)
-  }
-  println!("{}", read_to_string(args.get(1).unwrap()).unwrap());
+  let result = match read_to_string(args.get(1).unwrap()) {
+    Ok(text) => text,
+    Err(e) => format!("{:?}", e)
+  };
+  println!("{:?}", result);
   Ok(())
 }
